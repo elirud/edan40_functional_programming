e31f03d43f5c8c7cfdbfb2dad2fe791bab45b401
Elias Rudberg <eli.rud@hotmail.com>
2020-05-05 18:01:39 +0200

    Add fuctions to find and print alignments

--- assignment_2/allingment.hs
+++ assignment_2/allingment.hs
@@ -1,3 +1,5 @@
+type AlignmentType = (String,String)
+
 score :: Char -> Char -> Int
 score _ '-' = -2
 score '-' _ = -2
@@ -14,3 +16,27 @@ attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
 
 maximaBy :: Ord b => (a -> b) -> [a] -> [a]
 maximaBy f xs = [x | x <- xs, f x == maximum(map f xs)]
+
+optAlignments :: String -> String -> [AlignmentType]
+optAlignments [] [] = [([],[])]
+optAlignments (s:ps) [] = attachHeads x '-' (optAlignments xs [])
+optAlignments [] (p:ps) = attachHeads '-' y (optAlignments [] ys)
+optAlignments (s:ss) (p:ps) = maximaBy (uncurry similiarityScore) branches
+                           where branches = branch1 ++ branch2 ++ branch3
+                                 branch1 = attachHeads '-' p (optAlignments (s:ss) ps)
+                                 branch2 = attachHeads s '-' (optAlignments ss (p:ps))
+                                 branch3 = attachHeads s p (optAlignments ss ps)
+
+outputOptAlignments p s = showAlignments len alignments
+                        where alignments = optAlignments p s
+                              len        = length alignments
+
+showAlignments :: Int -> [(String, String)] -> IO()
+showAlignments n al = putStrLn $ formatAlignments al ++ "There are " ++ show n ++ " optimal alignment(s)!"
+
+formatAlignments :: [AlignmentType] -> String
+formatAlignments = concatMap (\(s,p) -> formatString s ++ "\n" ++ formatString p ++ "\n\n")
+
+formatString :: String -> String
+formatString "" = ""
+formatString (s:ss) = s : " " ++ formatString ss
