da2496d0eee83153dcaa8a06d51d131bc4c7d468
Elias Rudberg <eli.rud@hotmail.com>
2020-05-05 18:11:28 +0200

    Add unmodified mcsLength

--- assignment_2/allingment.hs
+++ assignment_2/allingment.hs
@@ -40,3 +40,20 @@ formatAlignments = concatMap (\(s,p) -> formatString s ++ "\n" ++ formatString p
 formatString :: String -> String
 formatString "" = ""
 formatString (s:ss) = s : " " ++ formatString ss
+
+mcsLength :: Eq a => [a] -> [a] -> Int
+mcsLength xs ys = mcsLen (length xs) (length ys)
+  where
+    mcsLen i j = mcsTable!!i!!j
+    mcsTable = [[ mcsEntry i j | j<-[0..]] | i<-[0..] ]
+       
+    mcsEntry :: Int -> Int -> Int
+    mcsEntry _ 0 = 0
+    mcsEntry 0 _ = 0
+    mcsEntry i j
+      | x == y    = 1 + mcsLen (i-1) (j-1)
+      | otherwise = max (mcsLen i (j-1)) 
+                        (mcsLen (i-1) j)
+      where
+         x = xs!!(i-1)
+         y = ys!!(j-1)
