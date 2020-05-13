7458ef58402b8d49f9d50f6584120bc1a757f443
Elias Rudberg <eli.rud@hotmail.com>
2020-05-13 14:45:09 +0200

    Add optimized lookup with tables.

--- assignment_2/allingment.hs
+++ assignment_2/allingment.hs
@@ -41,19 +41,54 @@ formatString :: String -> String
 formatString "" = ""
 formatString (s:ss) = s : " " ++ formatString ss
 
-mcsLength :: Eq a => [a] -> [a] -> Int
-mcsLength xs ys = mcsLen (length xs) (length ys)
+tableAlignment :: String -> String -> (Int, [AlignmentType])
+tableAlignment s p = (score, revl) 
+    where
+       revl       = map (\(a, b) -> (reverse a, reverse b)) l
+       (score, l) = alignmentScore (length s) (length p)
+       alignmentScore :: Int -> Int -> (Int, [AlignmentType])
+       alignmentScore i j = alignmentTable !! i !! j
+
+       alignmentTable :: [[(Int, [AlignmentType])]] 
+       alignmentTable = [[alignmentEntry i j | j <- [0..]] | i <- [0..]]
+
+       alignmentEntry :: Int -> Int -> (Int, [AlignmentType])
+       -- base cases
+       alignmentEntry 0 0 = (0, [("" , "")])
+       alignmentEntry i 0 = (i * scoreSpace, [(take i s, replicate i '-')])
+       alignmentEntry 0 j = (j * scoreSpace, [(replicate j '-', take j s)])
+       alignmentEntry i j
+            | x == y = (scoreMatch + s3 , attachHeads x y a3)
+            | otherwise = (fst (head list), concatMap snd list)
+                    where 
+                         list     = maximaBy fst [(s1 + scoreSpace, branch1),
+                                                  (s2 + scoreSpace, branch2),
+                                                  (s3 + scoreMismatch, branch3)] 
+                         (s1, a1) = alignmentScore i (j - 1)
+                         (s2, a2) = alignmentScore (i - 1) j
+                         (s3, a3) = alignmentScore (i - 1) (j - 1)
+                         branch1  = attachHeads '-' y  a1
+                         branch2  = attachHeads x '-'  a2 
+                         branch3  = attachHeads x y    a3 
+                         
+                         x = s !! (i-1)
+                         y = p !! (j-1)
+     
+tableScore :: String -> String -> Int
+tableScore s p = score (length s) (length p)
   where
-    mcsLen i j = mcsTable!!i!!j
-    mcsTable = [[ mcsEntry i j | j<-[0..]] | i<-[0..] ]
-       
-    mcsEntry :: Int -> Int -> Int
-    mcsEntry _ 0 = 0
-    mcsEntry 0 _ = 0
-    mcsEntry i j
-      | x == y    = 1 + mcsLen (i-1) (j-1)
-      | otherwise = max (mcsLen i (j-1)) 
-                        (mcsLen (i-1) j)
+    score i j = scoreTable !!i!!j
+    scoreTable :: [[Int]]
+    scoreTable = [[ scoreEntry j i | i <-[0..]] | j <-[0..] ]
+    scoreEntry :: Int -> Int -> Int
+    -- base cases
+    scoreEntry 0 0 = 0              -- Score 0 when length of both strings are 0
+    scoreEntry i 0 = scoreSpace * i -- Score space * i when right hand string is empty
+    scoreEntry 0 j = scoreSpace * j -- Score space * j when left hand string is empty 
+    scoreEntry i j 
+        | x == '-' || y == '-' = scoreSpace    + max (score i (j - 1)) (score (i - 1) j)
+        | x == y               = scoreMatch    + score (i - 1) (j - 1)
+        | otherwise            = scoreMismatch + score (i - 1) (j - 1)
       where
-         x = xs!!(i-1)
-         y = ys!!(j-1)
+         x = s !! (i-1)
+         y = p !! (j-1)
