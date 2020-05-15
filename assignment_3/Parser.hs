77734bc2836d9b0afd16917324bd6346504a3011
Elias Rudberg <eli.rud@hotmail.com>
2020-05-15 03:28:15 +0200

    Implement letter function

--- assignment_3/Parser.hs
+++ assignment_3/Parser.hs
@@ -29,7 +29,7 @@ token :: Parser a -> Parser a
 token m = m #- spaces
 
 letter :: Parser Char
-letter =  error "letter not implemented"
+letter =  (char ? isAlpha)
 
 word :: Parser String
 word = token (letter # iter letter >-> cons)
