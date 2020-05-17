6fc61ff0d95fe714bf726e36b566eb1523ffa185
Elias Rudberg <eli.rud@hotmail.com>
2020-05-17 17:27:22 +0200

    Add support for printing a program

--- assignment_3/Program.hs
+++ assignment_3/Program.hs
@@ -6,6 +6,6 @@ import Prelude hiding (return, fail)
 newtype T = Program [Statement.T]
 instance Parse T where
   parse = iter Statement.parse >-> Program
-  toString = error "Program.toString not implemented"
+  toString (Program stmts) = concatMap Statement.toString stmts
              
-exec (Program p) = Statement.exec p Dictionary.empty
+exec (Program stmts) = Statement.exec stmts Dictionary.empty
