88906e335ba4fc46396e6645892ce14587198f41
Elias Rudberg <eli.rud@hotmail.com>
2020-05-17 02:40:49 +0200

    Add exec cases for statements

--- assignment_3/Statement.hs
+++ assignment_3/Statement.hs
@@ -28,10 +28,20 @@ buildIf ((expr, doIf) doElse) = if expr doIf doElse
 buildWhile (expr, stmt) -> While expr stmt
 
 exec :: [T] -> Dictionary.T String Integer -> [Integer] -> [Integer]
-exec (If cond thenStmts elseStmts: stmts) dict input = 
-    if (Expr.value cond dict)>0 
+exec [] _ _ = []
+exec (Assignment str expr : ts) dict input = exec ts (Dictionary.insert (str, Expr.value expr dict) dict) input
+exec (Skip : ts) dict input = exec ts dict input
+exec (Begin [stmts] : ts) dict input = exec (stmts ++ ts) dict input
+exec (If cond thenStmts elseStmts : stmts) dict input = 
+    if (Expr.value cond dict) > 0 
     then exec (thenStmts: stmts) dict input
     else exec (elseStmts: stmts) dict input
+exec (While cond stmt : ts) dict input = 
+    if (Expr.value cond dict) > 0
+    then exec (stmt : (While cond stmt) : ts) dict input
+    else exec ts dict input
+exec (Read str : ts) dict (i:input) = exec ts (Dictionary.insert (str, i) dict) input
+exec (Write expr : ts) dict input = Expr.value expr dict : exec ts dict input
 
 instance Parse Statement where
   parse = assignment ! skipStatement ! beginStatement ! ifStatement ! whileStatement ! readStatement ! writeStatement
